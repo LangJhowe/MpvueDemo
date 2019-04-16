b0dd9d75b42ed98d4823fa297893f418f8e83fbc
LangJhowe <ail>
2019-04-16 23:22:27 +0800

    微信小程序获取用户信息,openId和unioid交由服务器处理

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <div @click="clickHandle">
+  <div>
 
-    <div class="userinfo" @click="bindViewTap">
+    <div class="userinfo">
       <img class="userinfo-avatar" v-if="userInfo.avatarUrl" :src="userInfo.avatarUrl" background-size="cover" />
       <img class="userinfo-avatar" src="/static/images/user.png" background-size="cover" />
 
@@ -9,27 +9,14 @@
         <card :text="userInfo.nickName"></card>
       </div>
     </div>
-
-    <div class="usermotto">
-      <div class="user-motto">
-        <card :text="motto"></card>
-      </div>
-    </div>
-
-    <form class="form-container">
-      <input type="text" class="form-control" :value="motto" placeholder="v-model" />
-      <input type="text" class="form-control" v-model="motto" placeholder="v-model" />
-      <input type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy" />
-    </form>
-
-    <a href="/pages/counter/main" class="counter">去往Vuex示例页面</a>
-
-    <div class="all">
-        <div class="left">
-        </div>
-        <div class="right">
-        </div>
-    </div>
+    <button @click="weixinLogin">微信登录</button>
+    <!-- 微信开发用bindgetuserinfo vue用@getuserinfo -->
+    <button open-type="getUserInfo" @getuserinfo="getUserInfo">获取用户信息</button>
+    <button open-type="openSetting">打开授权设置页</button>
+    <!-- <open-data type="groupName" open-gid="xxxxxx"></open-data> -->
+    <open-data type="userNickName"></open-data>
+    <open-data type="userAvatarUrl"></open-data>
+    <open-data type="userGender" lang="zh_CN"></open-data>
   </div>
 </template>
 
@@ -43,26 +30,82 @@ export default {
       userInfo: {
         nickName: 'mpvue',
         avatarUrl: 'http://mpvue.com/assets/logo.png'
-      }
+      },
+      noGetUserInfoAuth: false
     }
   },
 
   components: {
     card
   },
+  onShow () {
+    console.log('S')
+    this.getSetting()
+  },
+  computed: {
 
+  },
   methods: {
-    bindViewTap () {
-      const url = '../logs/main'
-      if (mpvuePlatform === 'wx') {
-        mpvue.switchTab({ url })
-      } else {
-        mpvue.navigateTo({ url })
-      }
+    getSetting () {
+      wx.getSetting({
+        success: (res) => {
+          if (res.authSetting['scope.userInfo']) {
+            wx.getUserInfo({
+              success: (res) => {
+                console.log('已授权', res.userInfo)
+                this.noGetUserInfoAuth = false
+              }
+            })
+          } else {
+            this.noGetUserInfoAuth = true
+            console.log('未授权')
+          }
+        }
+      })
     },
-    clickHandle (ev) {
-      console.log('clickHandle:', ev)
-      // throw {message: 'custom test'}
+    weixinLogin () {
+      wx.login({
+        success: (res) => {
+          console.log('res', res)
+          wx.getSetting({
+            success: (res) => {
+              console.log('setting', res)
+              if (res.authSetting['scope.userInfo']) {
+                wx.getUserInfo({
+                  success: (res) => {
+                    console.log('userInfo', res)
+                    this.userInfo = res.userInfo
+                  }
+                })
+              } else {
+                console.log('没有授权获取用户信息')
+              }
+            }
+          })
+        }
+      })
+    },
+
+    /**
+       * 该接口方法不能获取openId,unionId,根据官方文档，openId，unionId都交给开发者服务器处理
+       * 小程序登录文档https://developers.weixin.qq.com/miniprogram/dev/framework/open-ability/login.html
+       * 说明：
+       *   1、调用 wx.login() 获取 临时登录凭证code ，并回传到开发者服务器。
+       *   2、调用 auth.code2Session 接口，换取 用户唯一标识 OpenID 和 会话密钥 session_key。
+       * 之后开发者服务器可以根据用户标识来生成自定义登录态，用于后续业务逻辑中前后端交互时识别用户身份。
+       * 注意：
+       * 会话密钥 session_key 是对用户数据进行 加密签名 的密钥。为了应用自身的数据安全，开发者服务器不应该把会话密钥下发到小程序，也不应该对外提供这个密钥。
+       * 2、临时登录凭证 code 只能使用一次
+       */
+    getUserInfo (e) {
+      // 缺少openId,unionId
+      console.log(e.mp.detail)
+    },
+    canIUse (method) {
+      return wx.canIUse(method)
+    },
+    doLogin () {
+      console.log('doLogin')
     }
   },
 
@@ -90,37 +133,4 @@ export default {
   color: #aaa;
 }
 
-.usermotto {
-  margin-top: 150px;
-}
-
-.form-control {
-  display: block;
-  padding: 0 12px;
-  margin-bottom: 5px;
-  border: 1px solid #ccc;
-}
-.all{
-  width:7.5rem;
-  height:1rem;
-  background-color:blue;
-}
-.all:after{
-  display:block;
-  content:'';
-  clear:both;
-}
-.left{
-  float:left;
-  width:3rem;
-  height:1rem;
-  background-color:red;
-}
-
-.right{
-  float:left;
-  width:4.5rem;
-  height:1rem;
-  background-color:green;
-}
 </style>
---
+++ static/other/other.js
@@ -0,0 +1,12 @@
+var a = {
+  'openId': 'oRrdQt1fshQOF8PNl1_wol2G7aiI',
+  'nickName': 'Lmfao',
+  'gender': 1,
+  'city': '',
+  'province': '',
+  'country': '以色列',
+  'avatarUrl': 'http://thirdwx.qlogo.cn/mmopen/vi_32/fvV4LN7bkrdv1rZ0UIP1orQkBKza8Ir3N2LoSwLpVCqn2LclJtr8San5Sk1G4UV1L4cIiaukl9TKSQ6g7WCiaBqw/132',
+  'unionId': 'oU5Yyt9uvqnQoSoZyTbRqsB4xsNg'
+}
+
+export default a
